c157ca9033a305d9800d98235993e5a5e8c9932f
DeeptangshuSaha <deeptangshusaha62@gmail.com>
2025-10-07 10:50:06 +0530

    Added the install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+# Detect script location to run from anywhere
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $DOTFILES_DIR
+
+echo "🚀 Starting dotfiles setup..."
+
+# --- /etc configs ---
+if [ "$EUID" -ne 0 ]; then
+    echo "⚠️  Linking /etc configs requires sudo privileges"
+    sudo stow -d $DOTFILES_DIR -t /etc etc
+else
+    stow -d $DOTFILES_DIR -t /etc etc
+fi
+
+# --- ~/.config configs ---
+echo "🔗 Linking user configs..."
+stow -d $DOTFILES_DIR -t ~/.config .config
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+echo "✅ All configs linked successfully!"
+
